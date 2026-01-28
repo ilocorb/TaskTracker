4ab003882b44ee2512f446c71c509d4e4f535ecf
Patrick <patrick.piereder@gmx.com>
2026-01-28 19:57:05 +0100

    update sw

--- app/static/js/sw.js
+++ app/static/js/sw.js
@@ -1,4 +1,4 @@
-self.addEventListener("install", (event)=>{
+self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open('tasktracker-cache').then(cache => {
             return cache.addAll([
@@ -7,19 +7,21 @@ self.addEventListener("install", (event)=>{
                 '/base.html',
                 '/login.html',
                 '/register.html',
-                '/login.html',
+                '/users.html',
                 '/static/js/admin.js',
                 '/static/js/app.js',
                 '/static/admin.css',
                 '/static/desktop.css',
                 '/static/style.css',
-                '/static/icons/icon_T.png'
+                '/static/icons/icon-192.png',
+                '/static/icons/icon-512.png',
+                '/static/icons/favicon.png'
             ])
         })
     );
 });
 
-self.addEventListener('fetch', (event)=>{
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then(response => {
             response || fetch(event.request);
